5069e0c3b53b435be6922885a35ef88349fb6d6a
Vitalis Wiens <vw@metaphacts.com>
2023-07-28 17:05:57 +0200

    documentation updates
    minor documentation updates
      renamed transformer to transformation script
    minor configuration updates
      added schema:location range to be schema:Place

--- transformation-script/OwlProperty.py
+++ transformation-script/OwlProperty.py
@@ -18,16 +18,11 @@ class OwlProperty():
     self.propType=type
 
   def applyHeuristics(self):
-    print(self.uri, self.ranges, ">>>>>>>>>>>> ")
     while self.isMixed==True:
       for dataRange in self.ranges:
         if (dataRange in MAPPED_DATATYPES):
           self.ranges.remove(dataRange)
       self.checkForMultipleTypes()
-      
-      
-    print('<<<<<<,', self.ranges)
-       
       #select one of the ranges;
 
   
@@ -63,38 +58,7 @@ class OwlProperty():
         propDef+="<%s>, " % (sub)
       propDef = propDef[:-2]
 
-    #extract domain properties 
-    # if len(self.domains) > 0 : 
-    #   propDef+=";\n\t rdfs:domain "
-    #   for domain in self.domains: 
-    #     propDef+="<%s>, " % (domain)
-    #   propDef = propDef[:-2]
-
-   
-    #extract range properties 
-    # if len(self.ranges) > 0 : 
-    #   propDef+=";\n\t rdfs:range "
-    #   for range in self.ranges: 
-    #     if self.propType=='owl:DatatypeProperty':
-    #       #modify the schemaProp to xsd
-    #       propDef+="<%s>, " % (self.mapPropertyToXsd(range))
-    #     if self.propType=='owl:ObjectProperty':
-    #       #note here ranges could have also datatypes;
-    #       pureObjectProp = self.isPureObjectProp(self.ranges)
-    #       if pureObjectProp:
-    #         propDef+="<%s>, " % (range)
-    #       else:
-    #         propDef+="<MIXED%s>, " % (range)
-    #         self.isMixed=True
-    #   propDef = propDef[:-2]
-    #closing the prop def; 
+    
     propDef+=".\n\n"
     return propDef
-
-
-  def typeAssertionHeuristic(self):
-    print(self.label, "range Overview" )
-    if len(self.ranges) > 0 : 
-      for x in self.ranges:
-        print("\t\t",x)
 
